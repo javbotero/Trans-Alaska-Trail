98970cfb6690e48d5cf8e7e2419442d35eb0a3a6
Eli White <elibbw@gmail.com>
2016-03-29 01:03:53 -0400

    Mobile version

--- _site/js/script.js
+++ _site/js/script.js
@@ -2,12 +2,16 @@ $(function(){
   imageResize();
   var initial = 25;
   $( window ).scroll(function(){
-    var scroll = $(window).scrollTop();
-    var padding = initial - Math.floor(scroll / 4);
-    padding = padding < 0 ? 0 : padding;
-    $("#nav a").css("padding-top", padding);
-    $("#nav a").css("padding-bottom", padding);
-    imageResize();
+    if($( window ).width() > 970){
+      var scroll = $(window).scrollTop();
+      var padding = initial - Math.floor(scroll / 4);
+      padding = padding < 0 ? 0 : padding;
+      $("#nav a").css("padding-top", padding);
+      $("#nav a").css("padding-bottom", padding);
+      if (/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())) {
+        imageResize();
+      }
+    }
   })
 });
 
@@ -15,5 +19,6 @@ function imageResize(){
   var $logoEl = $('#logo');
   var imgRatio = 1.75; // hardcoded; could pull this out manually on load if desired
   var imgHeight = $logoEl.css('height').replace(/px/, '');
-  $logoEl.css('width', imgHeight * imgRatio + 'px');
+  var newWidth = imgHeight * imgRatio;
+  $logoEl.css('width',  + 'px');
 }

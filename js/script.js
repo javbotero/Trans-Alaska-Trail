6dc47ec952d76e15c4befc5f2f21a12e0af6ffa0
Eli White <elibbw@gmail.com>
2016-12-30 18:58:05 -0900

    Adjust for new logo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $(function(){
+  imageResize();
   var initial = 25;
   $( window ).scroll(function(){
     if($( window ).width() > 970){
       var scroll = $(window).scrollTop();
       var padding = initial - Math.floor(scroll / 4);
-      padding = padding < 0 ? 0 : padding;
+      padding = padding < 10 ? 10 : padding;
       $("#nav a").css("padding-top", padding);
       $("#nav a").css("padding-bottom", padding);
       imageResize();
@@ -14,8 +15,8 @@ $(function(){
 
 function imageResize(){
   var $logoEl = $('#logo');
-  var imgRatio = 1.75; // hardcoded; could pull this out manually on load if desired
-  var imgHeight = $logoEl.css('height').replace(/px/, '');
+  var imgRatio = 2.25; // hardcoded; could pull this out manually on load if desired
+  var imgHeight = $logoEl.height(); //('height').replace(/px/, '');
   var newWidth = imgHeight * imgRatio;
   $logoEl.css('width', newWidth + 'px');
 }
